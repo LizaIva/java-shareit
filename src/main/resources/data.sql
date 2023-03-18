eeb366a582d1eebd6ca28458b5f0c548869ce8af
LizaIva <ligamentum8@gmail.com>
2023-03-18 11:14:20 +0300

    refactor: переход на db h2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,5 @@
-INSERT INTO status (status_id, name)
+MERGE INTO status (status_id, name)
     VALUES (0, 'WAITING'),
     (1, 'APPROVED'),
     (2, 'REJECTED'),
-    (3, 'CANCELED')
-ON CONFLICT (status_id) DO NOTHING;
+    (3, 'CANCELED');
