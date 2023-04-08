4ac794ec6fb39d5044b2f9f3068d355b07d0671b
LizaIva <ligamentum8@gmail.com>
2023-04-08 18:32:51 +0300

    fix: data.sql

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -2,5 +2,4 @@ INSERT INTO status (status_id, name)
 VALUES (0, 'WAITING'),
        (1, 'APPROVED'),
        (2, 'REJECTED'),
-       (3, 'CANCELED')
-ON CONFLICT (status_id) DO NOTHING;
+       (3, 'CANCELED');
